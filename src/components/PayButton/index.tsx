f035a6383873aee8affd97e040350077b736bdb5
Gui-dev <adrianon10@gmail.com>
2021-02-07 03:04:04 -0300

    feat: create component PayButton

---
+++ src/components/PayButton/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import { Container } from './style'
+
+export const PayButton: React.FC = () => {
+  return (
+    <Container>
+      <Text style={{ color: '#FFF' }}>Pay Button</Text>
+    </Container>
+  )
+}
