8ca834fe0ca026475bb99ccaca0072fb28e01491
Gui-dev <adrianon10@gmail.com>
2021-02-07 03:02:56 -0300

    feat: create screens Home, Wallet and Pay

---
+++ src/screens/Pay/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+// import { Container } from './style'
+
+export const Pay: React.FC = () => {
+  return <Text>Pay</Text>
+}
---
+++ src/screens/Wallet/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './style'
+
+export const Wallet: React.FC = () => {
+  return <Text>Wallet</Text>;
+}
