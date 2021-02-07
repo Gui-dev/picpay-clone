c3e48684e6dc0ac46d077ab03bb389b46370ba7b
Gui-dev <adrianon10@gmail.com>
2021-02-07 03:02:19 -0300

    feat: add routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { AntDesign, Ionicons } from '@expo/vector-icons'
+
+import { PayButton } from './../components/PayButton'
+
+import { Home } from './../screens/Home'
+import { Wallet } from './../screens/Wallet'
+import { Pay } from './../screens/Pay'
+
+export const Routes: React.FC = () => {
+  const Tab = createBottomTabNavigator()
+
+  const icons = {
+    Home: {
+      lib: AntDesign,
+      name: 'home'
+    },
+    Wallet: {
+      lib: AntDesign,
+      name: 'creditcard'
+    },
+    Notifications: {
+      lib: Ionicons,
+      name: 'ios-notifications-outline'
+    },
+    Settings: {
+      lib: AntDesign,
+      name: 'setting'
+    },
+  } as any
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === 'Pay') {
+            return <PayButton />
+          }
+          const { lib: Icon, name } = icons[route.name]
+          return <Icon name={ name } size={ size } color={ color }/>
+        }
+      })}
+      tabBarOptions={{
+        style: {
+          backgroundColor: '#131418',
+          borderTopColor: 'rgba(255, 255, 255, 0.2)'
+        },
+        activeTintColor: '#FFF',
+        inactiveTintColor: '#92929C'
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={ Home }
+        options={{
+          title: 'Início'
+        }}
+      />
+      <Tab.Screen
+        name="Wallet"
+        component={ Wallet }
+        options={{
+          title: 'Carteira'
+        }}
+      />
+      <Tab.Screen
+        name="Pay"
+        component={ Pay }
+        options={{
+          title: ''
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={ Pay }
+        options={{
+          title: 'Notificações'
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={ Pay }
+        options={{
+          title: 'Ajustes'
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
