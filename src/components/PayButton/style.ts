89d4aab9def4a1bc692416a44392ea8571432f89
Gui-dev <adrianon10@gmail.com>
2021-02-07 03:38:18 -0300

    style: add styles to component

--- src/components/PayButton/style.ts
+++ src/components/PayButton/style.ts
@@ -1,5 +1,27 @@
 import styled from 'styled-components/native'
+import { LinearGradient } from 'expo-linear-gradient'
 
-export const Container = styled.View`
+interface ILabelProps {
+  focused: boolean
+}
 
+export const Container = styled.TouchableWithoutFeedback`
+  align-items: center;
+  justify-content: center;
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+`
+
+export const Content = styled(LinearGradient)`
+  align-items: center;
+  justify-content: center;
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+`
+
+export const Label = styled.Text<ILabelProps>`
+  font-size: 12px;
+  color: ${({ focused }) => focused ? '#00AC4A' : '#FFF'};
 `
