8e88aac38542ebd9e99d142f5864a049c0ad33f9
Gui-dev <adrianon10@gmail.com>
2021-02-07 03:37:52 -0300

    feat: add properties

--- src/components/PayButton/index.tsx
+++ src/components/PayButton/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
 
-import { Container } from './style'
+import { Container, Content, Label } from './style'
 
-export const PayButton: React.FC = () => {
+interface IPayButtonProps {
+  navigate: () => void
+  focused: boolean
+}
+
+export const PayButton: React.FC<IPayButtonProps> = ({ navigate, focused }) => {
   return (
-    <Container>
-      <Text style={{ color: '#FFF' }}>Pay Button</Text>
+    <Container onPress={ navigate }>
+      <Content
+        colors={
+          focused
+          ? ['#FFF', '#CCC']
+          : ['#00FC60', '#00AC4A']
+        }
+        start={ [1, 0.2] }
+      >
+        <MaterialIcons name="attach-money" size={30} color={ focused ? '#00AC4A' : '#FFF' }/>
+        <Label focused={ focused }>Pagar</Label>
+      </Content>
     </Container>
   )
 }
