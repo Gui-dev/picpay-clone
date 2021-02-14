2fb59466b0a895cbb750730c7779bfc7b6733f89
Gui-dev <adrianon10@gmail.com>
2021-02-14 03:43:34 -0300

    feat: add component Activities and style screen

---
+++ src/components/Activities/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Feather, MaterialCommunityIcons, AntDesign } from '@expo/vector-icons'
+
+import avatar from './../../assets/images/avatar.png'
+import { Container, Header, Title, Card, CardHeader, Avatar, Description, Bold,
+  CardBody, Username, CardFooter, Details, Value, Divider, Date, Actions, Options, OptionLabel
+} from './style'
+
+export const Activities: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <Title>Atividades</Title>
+      </Header>
+
+      <Card>
+        <CardHeader>
+          <Avatar source={ avatar }/>
+          <Description>
+            <Bold>Você</Bold> pagou a <Bold>@bruce_silva</Bold>
+          </Description>
+        </CardHeader>
+
+        <CardBody>
+          <Username>Gui Silva</Username>
+        </CardBody>
+
+        <CardFooter>
+          <Details>
+            <Value>R$ 2,543.00</Value>
+            <Divider />
+            <Feather name="lock" size={ 14 } color="#FFF"/>
+            <Date>há 2 dias atrás</Date>
+          </Details>
+
+          <Actions>
+            <Options>
+              <MaterialCommunityIcons name="comment-outline" size={ 14 } color="#FFF"/>
+              <OptionLabel>0</OptionLabel>
+            </Options>
+            <Options>
+              <AntDesign  name="hearto" size={ 14 } color="#FFF"/>
+              <OptionLabel>1</OptionLabel>
+            </Options>
+          </Actions>
+        </CardFooter>
+      </Card>
+    </Container>
+  )
+}
---
+++ src/components/Activities/style.ts
@@ -0,0 +1,99 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  margin-top: 25px;
+  padding: 0 16px;
+`
+
+export const Header = styled.View`
+`
+
+export const Title = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: #FFF;
+`
+
+export const Card = styled.View`
+  margin-top: 20px;
+  padding: 15px;
+  background-color: #1E222B;
+  border-radius: 8px;
+`
+
+export const CardHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+
+export const Avatar = styled.Image`
+
+`
+
+export const Description = styled.Text`
+  font-size: 16px;
+  color: #FFF;
+  margin-left: 15px;
+`
+
+export const Bold = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: #FFF;
+`
+
+export const CardBody = styled.View`
+  margin-top: 20px;
+`
+
+export const Username = styled.Text`
+  font-size: 20px;
+  color: #FFF;
+`
+
+export const CardFooter = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 15px;
+`
+
+export const Details = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+
+export const Value = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #F75175;
+`
+
+export const Divider = styled.View`
+  width: 1px;
+  height: 13px;
+  margin: 0 10px;
+  background-color: rgba(255, 255, 255, .4);
+`
+
+export const Date = styled.Text`
+  font-size: 16px;
+  color: #FFF;
+  margin-left: 5px;
+`
+
+export const Actions = styled.View`
+  flex-direction: row;
+`
+
+export const Options = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  margin-left: 15px;
+`
+
+export const OptionLabel = styled.Text`
+  font-size: 14px;
+  color: #FFF;
+  margin-left: 5px;
+`
+
